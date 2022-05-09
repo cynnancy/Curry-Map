cef84eea2262917d6793d63d7026bd54b902e5f3
Nancy Chang <91360712+cynnancy@users.noreply.github.com>
2022-05-09 11:08:23 +0800

    Add files via upload

--- Curryweb/Chiayi.php
+++ Curryweb/Chiayi.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Curry Map-Chiayi</title>
@@ -13,22 +15,26 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
+        <link href="css/styles.css" rel="stylesheet" /> 
+        <link rel="stylesheet" href="css/uikit.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>        
+
     <body>
         <header>
             <h1 class="site-heading text-center text-faded d-none d-lg-block">
                 <span class="site-heading-upper text-primary mb-3">Find Your Destined Curry</span>
-                <span class="site-heading-lower"><a class="nav-link text-uppercase" href="Curry.php">Curry Map</span>
+                <span class="site-heading-lower"><a class="nav-link text-uppercase" href='Curry.php'>Curry Map</span>
             </h1>
         </header>
         <!-- Navigation-->
+        <!-- uk-sticky將bar固定在網頁最上方 -->
         <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
             <div class="container">
-                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="Curry.php">Start Bootstrap</a>
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href='Curry.php'>Start Bootstrap</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto">
+                    <ul class="navbar-nav mx-auto" >
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Northern.html">Northern</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Central.html">Central</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Southern.html">Southern</a></li>
@@ -37,31 +43,37 @@
                 </div>
             </div>
         </nav>
-        <section class="page-section about-heading">
-            <div class="container">
-                <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="assets/img/About.jpg" alt="..." />
-                <div class="about-heading-content">
-                    <div class="row">
-                        <div class="col-xl-9 col-lg-10 mx-auto">
-                            <div class="bg-faded rounded p-5">
-                                <h2 class="section-heading mb-4">
-                                    <span class="section-heading-upper">Strong Taste, Strong Spices</span>
-                                    <span class="section-heading-lower">About Curry Map</span>
-                                </h2>
-                                <p>Founded in 2022 by Nancy Chang and Christing Wei.</p>
-                                <p class="mb-0"></p>
-                                <p>We're dedicated to finding the best curry, may more and more people find their
-                                    <em>destined curry.</em>
-                                    Join us for your delicious food routine.
-                                </p>
+        <!-- Section -->
+        <section class="page-section cta">
+            <div class=" container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <!-- <div class="cta-inner bg-faded text-center rounded"><span class="text-start">店名</span><span>嘉義市</span></div> -->
+                        <div class="cta-inner bg-faded text-center rounded" data-accordion-trigger="">
+                            <span>嘉義市 / 西區</span>
+                        <!-- <div class="lStore-info-title"> -->
+                            <h3>向午粮品 手作料理
+                            <button style="border:none" href="#toggle-animation" class="uk-button-link" type="button" uk-toggle="target: #toggle-animation; animation: uk-animation-fade"><i class="fa-solid fa-plus"></i></button></h3>
+                                <hr size="3" color="#411D00"> <!--中間分隔線-->
+                            <div id="toggle-animation" class="uk-card uk-card-default uk-card-body uk-margin-small" target="True">
+                                <!-- <i class="fa-solid fa-location-dot"></i> google map icon -->
+                                <a class="lStore-info-link" href="https://goo.gl/maps/T45zC7GNoaerhCgn9" target="_blank">600024嘉義市西區建成街20號</a>
+                            </div>
+                            <div id="toggle-animation" class="uk-card uk-card-default uk-card-body uk-margin-small">
+                                <a href="https://www.facebook.com/%E5%90%91%E5%8D%88%E7%B2%AE%E5%93%81-110761128126965/" target="_blank" class="uk-margin-small-right"><i class="fab fa-facebook-f"></i></a>
+                            </div>
+                            <div id="toggle-animation" class="uk-card uk-card-default uk-card-body uk-margin-small">
+                                <span class="uk-margin-small-right" uk-icon="reciver"></span>
+                                    <a href="tel:0979 570 668">0979 570 668</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>   
         </section>
+
         <footer class="footer text-faded text-center py-5">
-            <div class="container"><p class="m-0 small">Copyright &copy; CurryMap Naning</p></div>
+            <div class="container"><p class="m-0 small">Copyright &copy; CurryMap</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
